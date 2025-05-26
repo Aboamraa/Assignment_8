2e1584778b02291e799a15c8c28d53b616def21d
Ahmed Aboamra <ahmedaboamra.2024@gmail.com>
2025-05-27 02:51:33 +0300

    add more quotes

--- js/main.js
+++ js/main.js
@@ -5,31 +5,28 @@ newQuoteBtn.addEventListener("click", displayQuote);
 
 quotesList = [
   {
-    quote: "“Be yourself; everyone else is already taken.”",
-    author: "― Oscar Wilde",
+    quote: `“Be yourself; everyone else is already taken.”`,
+    author: `― Oscar Wilde`,
   },
   {
-    quote:
-      "“I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.”",
-    author: "― Marilyn Monroe",
+    quote: `“I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.”`,
+    author: `― Marilyn Monroe`,
   },
   {
-    quote: "“So many books, so little time.”",
-    author: "― Frank Zappa",
+    quote: `“So many books, so little time.”`,
+    author: `― Frank Zappa`,
   },
   {
-    quote:
-      "“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”",
-    author: "― Albert Einstein",
+    quote: `“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”`,
+    author: `― Albert Einstein`,
   },
   {
-    quote: "“A room without books is like a body without a soul.”",
-    author: "― Marcus Tullius Cicero",
+    quote: `“A room without books is like a body without a soul.”`,
+    author: `― Marcus Tullius Cicero`,
   },
   {
-    quote:
-      "“Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.”",
-    author: "― Bernard M. Baruch",
+    quote: `“Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.”`,
+    author: `― Bernard M. Baruch`,
   },
   {
     quote: `“You've gotta dance like there's nobody watching,
@@ -86,6 +83,42 @@ quotesList = [
   “I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.”
   `,
     author: "― Maya Angelou",
+  },
+  {
+    quote: `
+  “Friendship ... is born at the moment when one man says to another "What! You too? I thought that no one but myself . . .”
+  `,
+    author: "― C.S. Lewis, The Four Loves",
+  },
+  {
+    quote: `
+“To live is the rarest thing in the world. Most people exist, that is all.”
+  `,
+    author: "― Oscar Wilde",
+  },
+  {
+    quote: `
+“A friend is someone who knows all about you and still loves you.”
+  `,
+    author: "― Elbert Hubbard",
+  },
+  {
+    quote: `
+“Always forgive your enemies; nothing annoys them so much.”
+  `,
+    author: "― Oscar Wilde",
+  },
+  {
+    quote: `
+“Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.”  `,
+    author:
+      "― Martin Luther King Jr., A Testament of Hope: The Essential Writings and Speeches",
+  },
+  {
+    quote: `
+“Live as if you were to die tomorrow. Learn as if you were to live forever.”
+  `,
+    author: "― Mahatma Gandhi",
   },
 ];
 
